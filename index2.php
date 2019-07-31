996e4839b2606c4e60d45b3405e559e4f95c1b2a
[andob482] <[andob482@student.liu.se]>
2019-07-31 12:25:36 +0200

    teststar

---
+++ index2.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <?php
+   chdir('C:/xampp/htdocs/apache_project');
+    if (isset($_POST['call'])){
+    shell_exec('./test.sh');
+    echo "Du klarade det";
+    } else {
+        echo "Misslyckande";
+    }
+    echo getcwd();
+    ?>
+    <form method="POST">
+    <button name="call">Test</button>
+    </form>
+
+</body>
+</html>
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Du klarade det!"
