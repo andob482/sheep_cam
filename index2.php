3ddd450c91460cb2e03018cc06a736413ee65e15
[andob482] <[andob482@student.liu.se]>
2019-08-09 12:34:10 +0200

    knapp

--- index2.php
+++ index2.php
@@ -6,19 +6,28 @@
 </head>
 <body>
 
-    <?php
-   chdir('var/www/html');
-    if (isset($_POST['call'])){
-    shell_exec('./test.sh');
-    echo "Du klarade det";
-    } else {
-        echo "Misslyckande";
-    }
-    echo getcwd();
-    ?>
-    <form method="POST">
-    <button name="call">Test</button>
-    </form>
+<form method="GET" action="index.php">
+    <input type="submit" value="on" name="id">
+    <input type="submit" value="off" name="id">
+</form>
+
+   <?php
+if (isset($_GET['id'])){
+    $status = $_GET['id'];
+
+    switch ($status) {
+        case 'on':
+            echo "Du tryckte ON";
+            shell_exec("var/www/html/functions/test.sh");
+            break;
+        case 'off':
+            echo "Du tryckte OFF";
+            shell_exec("echo off 2>&1");
+            break; 
+}}
+
+?>
+    
 
 </body>
 </html>
